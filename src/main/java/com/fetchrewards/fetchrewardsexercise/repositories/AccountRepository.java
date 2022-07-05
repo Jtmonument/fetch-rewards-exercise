55af9068222677c52d0aa256fdf461821ebbd9c0
Jonathan Taylor <jontaylor091@gmail.com>
2022-07-05 01:17:11 -0500

    Added queries to update points. Closes #5

--- src/main/java/com/fetchrewards/fetchrewardsexercise/repositories/AccountRepository.java
+++ src/main/java/com/fetchrewards/fetchrewardsexercise/repositories/AccountRepository.java
@@ -1,7 +1,16 @@
 package com.fetchrewards.fetchrewardsexercise.repositories;
 
 import com.fetchrewards.fetchrewardsexercise.model.Account;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface AccountRepository extends CrudRepository<Account, Long> {
+
+    @Modifying
+    @Transactional
+    @Query("Update Account a set a.points=a.points+:points where a.id=:id")
+    void addPoints(@Param("id") Long id, @Param("points") Integer points);
 }
--- src/main/java/com/fetchrewards/fetchrewardsexercise/repositories/PayerRepository.java
+++ src/main/java/com/fetchrewards/fetchrewardsexercise/repositories/PayerRepository.java
@@ -1,7 +1,19 @@
 package com.fetchrewards.fetchrewardsexercise.repositories;
 
 import com.fetchrewards.fetchrewardsexercise.model.Payer;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 public interface PayerRepository extends CrudRepository<Payer, Long> {
+    Optional<Payer> findPayerByName(String name);
+
+    @Modifying
+    @Transactional
+    @Query("Update Payer p set p.points=p.points+:points where p.id=:id")
+    void addPoints(@Param("id") Long id, @Param("points") Integer points);
 }
