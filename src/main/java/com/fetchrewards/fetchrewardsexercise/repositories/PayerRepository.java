09ef864ef6e926e0f6bb51240db509f54794c8fb
Jonathan Taylor <jontaylor091@gmail.com>
2022-07-05 15:14:07 -0500

    Refactor payer repository

--- src/main/java/com/fetchrewards/fetchrewardsexercise/repositories/PayerRepository.java
+++ src/main/java/com/fetchrewards/fetchrewardsexercise/repositories/PayerRepository.java
@@ -1,5 +1,6 @@
 package com.fetchrewards.fetchrewardsexercise.repositories;
 
+import com.fetchrewards.fetchrewardsexercise.model.Account;
 import com.fetchrewards.fetchrewardsexercise.model.Payer;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -7,13 +8,15 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PayerRepository extends CrudRepository<Payer, Long> {
-    Optional<Payer> findPayerByName(String name);
+    Optional<Payer> findByName(String name);
 
+    List<Payer> findAll();
     @Modifying
     @Transactional
-    @Query("Update Payer p set p.points=p.points+:points where p.id=:id")
-    void addPoints(@Param("id") Long id, @Param("points") Integer points);
+    @Query("Update Payer p set p.points=p.points+:points where p.name=:name and p.account=:account")
+    void addPoints(@Param("account") Account account, @Param("name") String name, @Param("points") Integer points);
 }
