10efd7e852ce97f3162bca9417e44129ba63c506
Jonathan Taylor <jontaylor091@gmail.com>
2022-07-05 19:46:07 -0500

    Remove unused import

--- src/main/java/com/fetchrewards/fetchrewardsexercise/repositories/TransactionRepository.java
+++ src/main/java/com/fetchrewards/fetchrewardsexercise/repositories/TransactionRepository.java
@@ -1,6 +1,5 @@
 package com.fetchrewards.fetchrewardsexercise.repositories;
 
-import com.fetchrewards.fetchrewardsexercise.model.Account;
 import com.fetchrewards.fetchrewardsexercise.model.Transaction;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
