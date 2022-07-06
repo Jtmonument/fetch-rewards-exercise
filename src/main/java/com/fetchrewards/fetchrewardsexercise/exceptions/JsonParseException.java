33018b8e89442142d321c005bad43ad3f9ce87a6
Jonathan Taylor <jontaylor091@gmail.com>
2022-07-06 02:20:47 -0500

    Change inheritance to RuntimeException

--- src/main/java/com/fetchrewards/fetchrewardsexercise/exceptions/JsonParseException.java
+++ src/main/java/com/fetchrewards/fetchrewardsexercise/exceptions/JsonParseException.java
@@ -1,8 +1,6 @@
 package com.fetchrewards.fetchrewardsexercise.exceptions;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-public class JsonParseException extends JsonProcessingException {
+public class JsonParseException extends RuntimeException {
     public JsonParseException(String json) {
         super("Could not parse JSON '" + json + "'");
     }
