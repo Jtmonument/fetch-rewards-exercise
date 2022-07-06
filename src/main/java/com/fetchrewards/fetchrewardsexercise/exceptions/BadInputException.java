15a98c8ca6c7057dcffe9d3453beb1d6a0084c93
Jonathan Taylor <jontaylor091@gmail.com>
2022-07-06 02:20:02 -0500

    Add field value as parameter

--- src/main/java/com/fetchrewards/fetchrewardsexercise/exceptions/BadInputException.java
+++ src/main/java/com/fetchrewards/fetchrewardsexercise/exceptions/BadInputException.java
@@ -1,7 +1,8 @@
 package com.fetchrewards.fetchrewardsexercise.exceptions;
 
 public class BadInputException extends RuntimeException {
-    public BadInputException(String field) {
-        super("JSON field " + field + " is zero, null or of a bad format.");
+    public <T> BadInputException(String field, T value) {
+        super("JSON field " + field + " has value '" + value
+                + "', which is either zero, null, blank, or of a bad format.");
     }
 }
