80ad0d002b0742188be92a1f5808148eb52b8fb8
Jonathan Taylor <jontaylor091@gmail.com>
2022-07-13 13:59:33 -0500

    Refactor validations

--- src/main/java/com/fetchrewards/fetchrewardsexercise/exceptions/BadInputException.java
+++ src/main/java/com/fetchrewards/fetchrewardsexercise/exceptions/BadInputException.java
@@ -3,6 +3,6 @@ package com.fetchrewards.fetchrewardsexercise.exceptions;
 public class BadInputException extends RuntimeException {
     public <T> BadInputException(String field, T value) {
         super("JSON field " + field + " has value '" + value
-                + "', which is either zero, null, blank, or of a bad format.");
+                + "', which is either null, blank, negative, or of a bad format.");
     }
 }
